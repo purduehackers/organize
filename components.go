d19857f03e44469e26957a877e16e13350118d35
Matthew Stanciu <mattbstanciu@gmail.com>
2023-08-28 06:56:53 -0400

    format

--- components.go
+++ components.go
@@ -9,15 +9,25 @@ import (
 )
 
 func joinPurdueHackersView() string {
-	outerContainerStyle := lipgloss.NewStyle().Padding(1)
-	innerContainerStyle := lipgloss.NewStyle().Padding(0, 1).Background(lipgloss.Color("#fcd34d"))
-	textStyle := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#000000")).Blink(true)
+	outerContainerStyle := lipgloss.NewStyle().
+		Padding(1)
+	innerContainerStyle := lipgloss.NewStyle().
+		Padding(0, 1).
+		Background(lipgloss.
+		Color("#fcd34d"))
+	textStyle := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#000000")).
+		Blink(true)
 
 	return outerContainerStyle.Render(innerContainerStyle.Render(textStyle.Render("JOIN PURDUE HACKERS"))) + "\n"
 }
 
 func introDescriptionView(width int) string {
-	return lipgloss.NewStyle().Width(int(math.Round(float64(width)*0.6))).Padding(0, 1).Render("Purdue Hackers is a group of students who help each other build creative technical projects. We're looking for a few new organizers to join our team.\n\nGet started at the README. Use ↑ and ↓ to navigate.") + "\n\n"
+	return lipgloss.NewStyle().
+		Width(int(math.Round(float64(width)*0.6))).
+		Padding(0, 1).
+		Render("Purdue Hackers is a group of students who help each other build creative technical projects. We're looking for a few new organizers to join our team.\n\nGet started at the README. Use ↑ and ↓ to navigate.") + "\n\n"
 }
 
 func positionListItemView(str string, selected bool) string {
@@ -44,7 +54,11 @@ var (
 	headerStyle = func() lipgloss.Style {
 		b := lipgloss.RoundedBorder()
 		b.Right = "├"
-		return lipgloss.NewStyle().BorderStyle(b).BorderForeground(lipgloss.Color("#fcd34d")).Padding(0, 1).Bold(true)
+		return lipgloss.NewStyle().
+			BorderStyle(b).
+			BorderForeground(lipgloss.Color("#fcd34d")).
+			Padding(0, 1).
+			Bold(true)
 	}()
 
 	footerStyle = func() lipgloss.Style {
@@ -56,7 +70,9 @@ var (
 
 func (m model) headerView() string {
 	title := headerStyle.Render(m.selectedFileName)
-	line := strings.Repeat(lipgloss.NewStyle().Foreground(lipgloss.Color("#fcd34d")).Render("─"), Max(0, m.viewport.Width-lipgloss.Width(title)))
+	line := strings.Repeat(lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#fcd34d")).
+		Render("─"), Max(0, m.viewport.Width-lipgloss.Width(title)))
 	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
 }
 
@@ -64,7 +80,9 @@ func (m model) footerView() string {
 	helpView := lipgloss.PlaceHorizontal(m.viewport.Width, lipgloss.Right, m.help.View(m.keys))
 
 	info := footerStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
-	line := strings.Repeat(lipgloss.NewStyle().Foreground(lipgloss.Color("#fcd34d")).Render("─"), Max(0, m.viewport.Width-lipgloss.Width(info)))
+	line := strings.Repeat(lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#fcd34d")).
+		Render("─"), Max(0, m.viewport.Width-lipgloss.Width(info)))
 	footerInfo := lipgloss.JoinHorizontal(lipgloss.Center, line, info)
 
 	return helpView + "\n" + footerInfo
