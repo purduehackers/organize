9f6847263bd04e6a58ea9170aae2fa83a6b294b9
Matthew Stanciu <mattbstanciu@gmail.com>
2023-08-29 10:46:32 -0400

    Increase timeout

--- main.go
+++ main.go
@@ -88,7 +88,7 @@ func main() {
 
 	<-done
 	log.Info("Stopping SSH server")
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
 		log.Error("could not stop server", "error", err)
